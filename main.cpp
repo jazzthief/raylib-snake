cf1003918c254851652d56f60dc41205e0fd4f6f
jazzthief <mynameisyegor@gmail.com>
2023-11-20 22:42:07 +0100

    Finish collision; add basic UI

--- main.cpp
+++ main.cpp
@@ -13,13 +13,12 @@ Color darkGreen = {43, 51, 24, 255};
 // Cell-based rendering
 const int cellSize = 30;
 const int cellCount = 25;
+const int borderWidth = 75;  // px
+// Playable area
 const int screenWidth = cellSize * cellCount;
 const int screenHeight = cellSize * cellCount;
 
 double lastUpdateTime = 0;
-
-// Local functions
-static void UpdateFrame(void); // Update and draw one frame
 
 // Check if `interval` (seconds) has passed
 static bool updateTriggered(double interval)
@@ -56,8 +55,14 @@ public:
 
     void draw()
     {
-        // Bad representation for collision?
-        DrawCircle(pos.x * cellSize, pos.y * cellSize, cellSize / 2, darkGreen);
+        Rectangle rect = {
+            borderWidth + pos.x * cellSize,
+            borderWidth + pos.y * cellSize,
+            (float)cellSize,
+            (float)cellSize
+        };
+        // using this to draw a circle because it's easier to position
+        DrawRectangleRounded(rect, 1, 6, darkGreen);
     }
 
     Vector2 generateRandomPos()
@@ -88,6 +93,7 @@ public:
         Vector2{4, 9}
     };
     Vector2 direction = {0, -1};
+    bool shouldGrow = false;
 
     Snake()  // init 3-cell snake in the center
     {   
@@ -103,19 +109,26 @@ public:
         for(int i = 0; i < body.size(); i++)
         {
             Rectangle segment = {
-                body[i].x * cellSize,
-                body[i].y * cellSize,
+                borderWidth + body[i].x * cellSize,
+                borderWidth + body[i].y * cellSize,
                 (float)cellSize,
-                (float)cellSize,
+                (float)cellSize
             };
             DrawRectangleRounded(segment, 0.5, 6, darkGreen);
         }
     }
 
-    void move()
+    void update()
     {
-        body.pop_back();
         body.push_front(Vector2Add(body[0], direction));
+        if (shouldGrow) shouldGrow = false;
+        else body.pop_back();
+    }
+
+    void reset()
+    {
+        body = {Vector2{6, 9}, Vector2{5, 9}, Vector2{4, 9}};
+        direction = {1, 0};
     }
 };
 
@@ -124,6 +137,8 @@ class Game
 public:
     Snake snake = Snake();
     Food food = Food(snake.body);
+    bool running = true;
+    int score = 0;
 
     void draw()
     {
@@ -133,23 +148,52 @@ public:
 
     void update()
     {
-        snake.move();
-        checkCollisionFood();
+        if (running)
+        {
+            snake.update();
+            checkCollisionFood();
+            checkCollisionBody();
+            checkCollisionEdge();
+        }
     }
 
     void checkCollisionFood()
     {
         if (Vector2Equals(snake.body[0], food.pos))
         {
+            snake.shouldGrow = true;
             food.pos = food.spawnRandom(snake.body);
+            score++;
         }
+    }
+
+    void checkCollisionBody()
+    {
+        // Don't copy the deque?
+        deque<Vector2> bodyWithoutHead = snake.body;
+        bodyWithoutHead.pop_front();
+        if (isSnakeBody(snake.body[0], bodyWithoutHead)) gameOver();
+    }
+
+    void checkCollisionEdge()
+    {
+        if (snake.body[0].x == cellCount || snake.body[0].x == -1) gameOver();
+        if (snake.body[0].y == cellCount || snake.body[0].y == -1) gameOver();
+    }
+
+    void gameOver()
+    {
+        snake.reset();
+        food.pos = food.spawnRandom(snake.body);
+        running = false;
+        score = 0;
     }
 };
 
 int main()
 {
     cout << "Starting the game..." << endl;
-    InitWindow(screenWidth, screenHeight, "Snake");
+    InitWindow(2 * borderWidth + screenWidth, 2 * borderWidth + screenHeight, "Snake");
     SetTargetFPS(60);
 
     Game game = Game();
@@ -163,13 +207,39 @@ int main()
 
         // Controls
         // pointer to direction?
-        if (IsKeyDown(KEY_RIGHT) && game.snake.direction.x != -1) game.snake.direction = {1, 0};
-        if (IsKeyDown(KEY_LEFT) && game.snake.direction.x != 1) game.snake.direction = {-1, 0};
-        if (IsKeyDown(KEY_UP) && game.snake.direction.y != 1) game.snake.direction = {0, -1};
-        if (IsKeyDown(KEY_DOWN) && game.snake.direction.y != -1) game.snake.direction = {0, 1};
+        if (IsKeyDown(KEY_RIGHT) && game.snake.direction.x != -1)
+        {
+            game.snake.direction = {1, 0};
+            game.running = true;
+        }
+        if (IsKeyDown(KEY_LEFT) && game.snake.direction.x != 1)
+        {
+            game.snake.direction = {-1, 0};
+            game.running = true;
+        }
+        if (IsKeyDown(KEY_UP) && game.snake.direction.y != 1)
+        {
+            game.snake.direction = {0, -1};
+            game.running = true;
+        }
+        if (IsKeyDown(KEY_DOWN) && game.snake.direction.y != -1)
+        {
+            game.snake.direction = {0, 1};
+            game.running = true;
+        }
 
         ClearBackground(green);
-        DrawFPS(10, 10);
+
+        // Interface
+        Rectangle borderRectangle = {
+            (float)borderWidth - 5,
+            (float)borderWidth - 5,
+            (float)cellSize * cellCount + 10,
+            (float)cellSize * cellCount + 10
+        };
+        DrawRectangleLinesEx(borderRectangle, 5, darkGreen);
+        DrawFPS(borderWidth + screenWidth - 10, 10);
+        DrawText(TextFormat("%03i", game.score), borderWidth - 5, 20, 40, darkGreen);
         game.draw();
 
         EndDrawing();
